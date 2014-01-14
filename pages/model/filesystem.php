6257174874dbf6be4a15dce88870f5e72c89fad6
Jörg Bachmann <bachmjoe@informatik.hu-berlin.de>
2014-01-14 20:29:29 +0100

    Exception handling in filesystem

--- pages/model/filesystem.php
+++ pages/model/filesystem.php
@@ -7,25 +7,30 @@ function getUploadsMap( &$conflictsMap ) {
     global $mediaPath;
     $map = Array();
 
-    $directoryIterator = new RecursiveDirectoryIterator( $mediaPath );
-    foreach( new RecursiveIteratorIterator( $directoryIterator ) as $pathIterator )
-    {
-        if( !is_file( $pathIterator ) )
-            continue;
+    try {
+        $directoryIterator = new RecursiveDirectoryIterator( $mediaPath );
+        foreach( new RecursiveIteratorIterator( $directoryIterator ) as $pathIterator )
+        {
+            if( !is_file( $pathIterator ) )
+                continue;
 
-        $pi = pathinfo( $pathIterator );
-        $file = $pi[ 'basename' ];
-        $path = $pi[ 'dirname' ]."/".$file;
+            $pi = pathinfo( $pathIterator );
+            $file = $pi[ 'basename' ];
+            $path = $pi[ 'dirname' ]."/".$file;
 
-        if( array_key_exists( $file, $map ) ) {
-            if( !array_key_exists( $file, $conflictsMap ) ) {
-                $conflictsMap[ $file ] = Array( $map[ $file ] );
+            if( array_key_exists( $file, $map ) ) {
+                if( !array_key_exists( $file, $conflictsMap ) ) {
+                    $conflictsMap[ $file ] = Array( $map[ $file ] );
+                }
+
+                array_push( $conflictsMap[ $file ], $path );
             }
 
-            array_push( $conflictsMap[ $file ], $path );
+            $map[ $file ] = $path;
         }
-
-        $map[ $file ] = $path;
+    }
+    catch( Exception $e ) {
+        print( $e->getMessage() );
     }
 
     return $map;
